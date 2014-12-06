479537158eb707bb6bcbcb579f8a9f0bbce91d6b
Brennan Brown <bbrown17@punahou.edu>
2014-12-05 16:25:06 -1000

    Completed gameOver Death, fixed indenting
    I fixed indenting and made it so that the snake hitting itself made a
    gameOver screen.

--- MySnake.ino
+++ MySnake.ino
@@ -28,6 +28,7 @@
    
 // Arrays Tutorial: http://arduino.cc/en/Reference/Array
 
+// DID YOU KNOW THAT YOU CAN SELECT A BUNCH OF CODE AND INDENT IT ALL AT ONCE??
 
 #include <MeggyJrSimple.h>    // Required code, line 1 of 2.
 
@@ -36,6 +37,7 @@ int dir = 0;  // top of compass, possible values are 0, 90, 180, 270
 int xapple = random(8);
 int yapple = random(8);
 int marker = 4; // same number as points in array
+boolean gameOver= false;
 
 struct Point
 {
@@ -71,99 +73,131 @@ void setup()                    // run once, when the sketch starts
 
 void loop()  // run over and over again
 {
- // see variable data 
- updateSnake(); // must be at top of loop so 0,0 glitch doesn't happen
- Serial.print("x is");  // things in quotes show up literally
- Serial.println(p1.x);  // println is print line
- Serial.print("y is ");
- Serial.println(p1.y);
- Serial.println();
- 
- drawSnake();
- DisplaySlate();
- delay(250);
- ClearSlate(); 
+  if (gameOver == false)
+  {
+    
+   // see variable data 
+   updateSnake(); // must be at top of loop so 0,0 glitch doesn't happen
+   Serial.print("x is");  // things in quotes show up literally
+   Serial.println(p1.x);  // println is print line
+   Serial.print("y is ");
+   Serial.println(p1.y);
+   Serial.println();
  
  
-CheckButtonsPress();
+   drawSnake();
+   DisplaySlate();
+   delay(250);
+   ClearSlate(); 
  
- if (Button_Right)
- {
-   dir = 90;
- }
  
- if (Button_Left)
- {
-   dir = 270;
- }
+  CheckButtonsPress();
  
- if (Button_Up)
- {
-   dir = 0;
- }
+   if (Button_Right)
+   {
+     dir = 90;
+   }
  
- if (Button_Down)
- {
-   dir = 180;
- }
+   if (Button_Left)
+   {
+     dir = 270;
+   }
  
- if (dir == 90)
- {
-   snakeArray[0].x++;
- }
+   if (Button_Up)
+   {
+     dir = 0;
+   }
  
- if (dir == 270)
- {
-   snakeArray[0].x--;
- }
+   if (Button_Down)
+   {
+     dir = 180;
+   }
  
- if (dir == 0)
- {
-   snakeArray[0].y++;
- }
+   if (dir == 90)
+   {
+     snakeArray[0].x++;
+   }
  
- if (dir == 180)
- {
-   snakeArray[0].y--;
- }
+   if (dir == 270)
+   {
+     snakeArray[0].x--;
+   }
  
- if (snakeArray[0].x > 7)
- {
-   snakeArray[0].x = 0;
- }
+   if (dir == 0)
+   {
+     snakeArray[0].y++;
+   }
  
- if (snakeArray[0].y > 7)
- {
-   snakeArray[0].y = 0;
- }
+   if (dir == 180)
+   {
+     snakeArray[0].y--;
+   }
  
- if (snakeArray[0].x < 0)
- {
-   snakeArray[0].x = 7;
- }
+   if (snakeArray[0].x > 7)
+   {
+     snakeArray[0].x = 0;
+   }
  
- if (snakeArray[0].y < 0)
- {
-   snakeArray[0].y = 7;
- }
+   if (snakeArray[0].y > 7)
+   {
+     snakeArray[0].y = 0;
+   }
  
- DrawPx (xapple,yapple,DimRed);
+   if (snakeArray[0].x < 0)
+   {
+     snakeArray[0].x = 7;
+   }
  
- if (xapple == snakeArray[0].x) // collision detection (checks if snake hit apple)
- {
-    if (yapple == snakeArray[0].y) // xapple == snakeArray[0].x must be true as well
-    { 
+   if (snakeArray[0].y < 0)
+   {
+     snakeArray[0].y = 7;
+   }
+ 
+   DrawPx (xapple,yapple,DimRed);
+ 
+   if (xapple == snakeArray[0].x) // collision detection (checks if snake hit apple)
+   {
+      if (yapple == snakeArray[0].y) // xapple == snakeArray[0].x must be true as well
+      { 
       
-      Tone_Start(ToneA4, 100); delay(125); // apple eaten music
-      Tone_Start(ToneCs5, 100); delay(125);
-      Tone_Start(ToneE5, 100); delay(125);
+        Tone_Start(ToneA4, 100); delay(125); // apple eaten music
+        Tone_Start(ToneCs5, 100); delay(125);
+        Tone_Start(ToneE5, 100); delay(125);
+        
+        xapple = random(8); // generate new apple
+        yapple = random(8);
       
-      xapple = random(8); // generate new apple
-      yapple = random(8);
+        marker++;    
+      }  
+   } 
+ 
+  if (snakeCollide())
+  {
+    gameOver = true;
+  }
+  }
   
-      marker++;    
-    }  
- } 
+  else // gameOver content in here
+  {
+    Tone_Start(ToneB3, 250); // beeping noise with blinking X
+    
+    DrawPx(5,5,Red); DrawPx(6,6,Red); // blinking red X
+    DrawPx(2,5,Red); DrawPx(1,6,Red);
+    
+    DrawPx(3,4,Red); DrawPx(4,4,Red); 
+    DrawPx(3,3,Red); DrawPx(4,3,Red);
+    
+    DrawPx(5,2,Red); DrawPx(6,1,Red);
+    DrawPx(2,2,Red); DrawPx(1,1,Red);
+     
+    DisplaySlate();
+    delay(250);
+    
+    ClearSlate(); // creates blinking effect
+    DisplaySlate();
+    delay(250);
+  }
+    
  
 }  
 
@@ -173,6 +207,7 @@ void drawSnake()
   {
     DrawPx(snakeArray[i].x,snakeArray[i].y,Green);  
   }
+  
 }
 
 void updateSnake()
@@ -181,4 +216,16 @@ void updateSnake()
   {
     snakeArray[i] = snakeArray [i-1];
   }
+}
+
+boolean snakeCollide()
+{
+  for (int i = 1; i < marker; i++) // ends game if snake hits itself
+  {
+    if (snakeArray[0].x == snakeArray[i].x &&
+      snakeArray[0].y == snakeArray[i].y)
+   
+    return true;
+  }
+    return false;
 }
